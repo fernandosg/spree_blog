091a37cbe396d0395d7d95b0ac78c5c04ca75191
fernandosegurabigo <fernandosegom@gmail.com>
2019-04-14 16:22:42 -0500

    Ajuste en la definición de relaciones. Se agregan delegates

--- app/models/spree/article.rb
+++ app/models/spree/article.rb
@@ -1,4 +1,6 @@
-class Spree::Article < Spree::Base
-  belongs_to :user, foreign_key: 'spree_user_id', class_name: 'Spree::User', inverse_of: :spree_article, optional: true
-  belongs_to :article_category, foreign_key: 'article_category_id', class_name: 'Spree::ArticleCategory', inverse_of: :spree_article, optional: true
+class Spree::Article < ApplicationRecord
+  belongs_to :user, foreign_key: 'user_id', class_name: 'Spree::User'
+  belongs_to :article_category, foreign_key: 'article_category_id', class_name: 'Spree::ArticleCategory'
+  delegate :full_name, to: :user, prefix: 'get_author'
+  delegate :name, to: :article_category, prefix: 'get_category'
 end
